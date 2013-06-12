cf084d1c316b44ccf8bc90f14b6e9aac33df11d1
Madhumitha Viswanathan <madhuvishy@gmail.com>
2013-06-12 15:00:13 +0530

    Moving db params to config

--- __init__.py
+++ __init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 import logging
+import config
+import MySQLdb
 
 app = Flask(__name__)
-filehandler = logging.FileHandler(filename='/home/ssngeek/logs/user/error_cg.log')
+app.config.from_object('chennaigeeks.config')
+filehandler = logging.FileHandler(filename=config.LOG_FILE_LOC)
 filehandler.setLevel(logging.INFO)
 app.logger.addHandler(filehandler)
 
+db = MySQLdb.connect(user=config.DB_USERNAME, passwd=config.DB_PASSWORD, db=config.DB_NAME)
+
 from chennaigeeks import search
 
-if __name__ == '__main__':
-    app.run()
+#Uncomment on server
+#if __name__ == '__main__':
+#    app.run()
 
---
+++ default_config.py
@@ -0,0 +1,9 @@
+#Logging
+LOG_FILE_LOC = '/home/madhu/logs/error.log'
+
+#Database
+DB_USERNAME = ''
+DB_PASSWORD = ''
+DB_SERVER = ''
+DB_NAME = ''
+
